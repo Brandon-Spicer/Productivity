7e76e1bb893e74847e359a1cdc70f16e24d993f4
Brandon <brandon.david.spicer@gmail.com>
2020-07-17 15:50:35 -0700

    Prevent tosql.py from adding duplicates

--- tosql/tosql.py
+++ tosql/tosql.py
@@ -1,5 +1,8 @@
 import sqlite3
 
+"""
+https://docs.python.org/3/library/sqlite3.html
+"""
 
 conn = sqlite3.connect('test.db')
 c = conn.cursor()
@@ -16,35 +19,50 @@ c.execute('CREATE TABLE IF NOT EXISTS tasks (d DATE, start TIME, end TIME, name 
 # loop through day directories 
 for day in weekdays:
 
-	# open log file for today
-	with open(f'{path}/{day}/log', 'r' ) as f:
+    # open log file for today
+    with open(f'{path}/{day}/log', 'r' ) as f:
 
-		# get the date from first line
-		first_line = f.readline()
-		if len(first_line) == 1:
-			task_date = first_line[0]
-		else:
-			task_date = first_line.split()[-1]
+        # get the date from first line
+        first_line = f.readline()
+        if len(first_line) == 1:
+            task_date = first_line[0]
+        else:
+            task_date = first_line.split()[-1]
 
-		# get remaining lines
-		lines = f.readlines()
+        # TEST
+        print(task_date)
 
-		# break if file is empty
-		if len(lines) == 0:
-			break
+        # get remaining lines
+        lines = f.readlines()
 
-		# make task list
-		tasks = []
-		for line in lines:
-			tasks.append([task_date] + list(map(lambda x : x.strip(), line.split(sep='|'))))
+        # TEST
+        print(lines)
 
-		for task in tasks:
-			print(task)
+        # if there is any data, process it
+        if len(lines) > 0:
 
-		# add every task in the log to the database
-		for task in tasks:
-			command = '''INSERT INTO tasks VALUES (?,?,?,?,?,?,?,?)'''
-			c.execute(command, task) 
+            # make task list
+            tasks = []
+            for line in lines:
+                tasks.append([task_date] + list(map(lambda x : x.strip(), line.split(sep='|'))))
+
+            # TEST
+            for task in tasks:
+                print(task)
+
+            # add each task in the log to the database if it doesn't already exist
+            for task in tasks:
+
+                # check if an entry for the task already exists
+                c.execute('SELECT * FROM tasks WHERE (d=? AND start=? AND end=?\
+                        AND name=? AND total=? AND hours=? AND minutes=? AND note=?)', task)
+                entry = c.fetchone()
+
+                if entry is None:
+                    command = '''INSERT INTO tasks VALUES (?,?,?,?,?,?,?,?)'''
+                    c.execute(command, task) 
+                else:
+                    print('Entry already exists for this task.')
 
 conn.commit()
 conn.close()
